792dd73774e24813fbaf67c47da67c5d8a724710
HafiCode02 <hafizcode02@gmail.com>
2020-12-31 13:06:10 +0700

    adding greetings and save profile

--- app/Gateway/UserGateway.php
+++ app/Gateway/UserGateway.php
@@ -15,4 +15,26 @@ class UserGateway
     {
         $this->db = app('db');
     }
+
+    public function getUser(string $userId)
+    {
+        $user = $this->db->table('users')
+            ->where('user_id', $userId)
+            ->first();
+
+        if (!$user) {
+            return (array) $user;
+        }
+
+        return null;
+    }
+
+    public function saveUser(string $userId, string $displayName)
+    {
+        $this->db->table('users')
+            ->insert([
+                'user_id' => $userId,
+                'displayName' => $displayName
+            ]);
+    }
 }
--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -82,6 +82,79 @@ class Webhook extends Controller
         $signature = $this->request->server('HTTP_X_LINE_SIGNATURE') ?: '-';
         $this->logGateway->saveLog($signature, json_encode($body, true));
 
-        // return $this->handleEvents();
+        return $this->handleEvents();
+    }
+
+    private function handleEvents()
+    {
+        $data = $this->request->all();
+
+        if (is_array($data['events'])) {
+            foreach ($data['events'] as $event) {
+                if (!isset($event['source']['userId'])) continue;
+
+                $this->user = $this->userGateway->getUser($event['source']['userId']);
+
+                if (!$this->user) $this->followCallback($event);
+                else {
+                    if ($event['type'] == 'message') {
+                        if (method_exists($this, $event['message']['type'] . 'Message')) {
+                            $this->{$event['message']['type'] . 'Message'}($event);
+                        }
+                    } else {
+                        if (method_exists($this, $event['type'] . 'Callback')) {
+                            $this->{$event['message']['type'] . 'Callback'}($event);
+                        }
+                    }
+                }
+            }
+        }
+
+        $this->response->setContent("No Events Found!");
+        $this->response->setStatusCode(200);
+        return $this->response;
+    }
+
+    private function followCallback($evt)
+    {
+        $res = $this->bot->getProfile($evt['source']['userId']);
+        if ($res->isSucceeded()) {
+            $profile = $res->getJSONDecodedBody();
+
+            $message = "Salam Kenal, " . $profile['displayName'] . "!\n";
+            $message .= "Selamat Datang di Line Bot Tebak Gambar \n";
+            $message .= "Kamu bisa memilih Level Tebak Gambar yang ada di Bot ini \n";
+            $message .= "Saat ini Level yang tersedia adalah 1 - 2";
+            $message .= "Untuk Memulai Kamu bisa Menggunakan Perintah \"/start level 1\" untuk memulai Bot";
+
+            $textMessageBuilder = new TextMessageBuilder($message);
+
+            $message2 = "Game ini juga ditunjukan sebagai penghibur di kala pandemi ini.";
+            $message2 .= "di bot ini juga kalian bisa mengecek Statistik Virus Covid-19 di Indonesia, dengan query";
+            $message2 .= "1. \"/coronastats\" untuk melihat data penyebaran Covid-19 di Indonesia";
+            $message2 .= "2. \"/coronastats provinsi\" untuk melihat data penyebaran di wilayah provinsi";
+            $message2 .= "3. \"/coronastats provinsi kota\" untuk melihat data penyebaran di wilayah kota";
+
+            $textMessageBuilder2 = new TextMessageBuilder($message2);
+
+            $message3 = "Selamat Bersenang senang";
+
+            $textMessageBuilder3 = new TextMessageBuilder($message3);
+
+            $stickerMessageBuilder = new StickerMessageBuilder(1, 3);
+
+            $multiMessageBuilder = new MultiMessageBuilder();
+            $multiMessageBuilder->add($textMessageBuilder);
+            $multiMessageBuilder->add($textMessageBuilder2);
+            $multiMessageBuilder->add($textMessageBuilder3);
+            $multiMessageBuilder->add($stickerMessageBuilder);
+
+            $this->bot->replyMessage($evt['replyToken'], $multiMessageBuilder);
+
+            $this->userGateway->saveUser(
+                $profile['userId'],
+                $profile['displayName']
+            );
+        }
     }
 }
